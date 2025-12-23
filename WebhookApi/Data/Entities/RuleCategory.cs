63d59d65c93bfcc20635ecb8cff7a8d1d6f7fd73
Emmanuel Jomer Baling <ej.baling@gmail.com>
2025-12-24 10:54:21 +1300

    move common classes into nuget package

--- WebhookApi/Data/Entities/RuleCategory.cs
+++
@@ -1,9 +0,0 @@
-namespace RedwoodIloilo.Common.Entities;
-
-public class RuleCategory
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public string? Description { get; set; }
-    public ICollection<Rule> Rules { get; set; } = new List<Rule>();
-}
